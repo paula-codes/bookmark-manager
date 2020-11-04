e90b8fe251a0ba25b6dc8a3c2756655172b16046
Paula Gavenyte <paula.gavenyte@gmail.com>
2020-11-04 11:00:17 +0000

    Delete feature fixed

--- spec/features/delete_bookmark_spec.rb
+++ spec/features/delete_bookmark_spec.rb
@@ -3,7 +3,7 @@ feature 'Deleting bookmarks' do
     Bookmark.add(url: 'http://www.makersacademy.com', title: 'Makers Academy')
     visit '/bookmarks'
     expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
-    first('.bookmark').click_button 'Delete'
+    click_button 'Delete'
     expect(current_path).to eq '/bookmarks'
     expect(page).not_to have_link('Makers Academy', href: 'http://www.makersacademy.com')
   end
